85d7e3d417d47cb55b76cf0e959f89562283eef1
FrancescoDale <fd060862@gmail.com>
2021-01-07 17:12:47 +0100

    menu navbar + favicon e fix css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('page-title', 'DC Comics - Home')
+
 @section('content')
     <h1>prova</h1>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,12 +6,56 @@
                     <a class="text-uppercase" href="#">dc power visa</a>
                 </li>
                 <li>
-                    <a class="text-uppercase" href="#">additional dc sites </a>
+                    <a class="text-uppercase" href="#">additional dc sites <span> &#9660; </span> </a>
                 </li>
             </ul>
         </nav>
     </div>
     <div class="header-navbar">
+        <div class="header-wrapper">
+            <div class="logo">
+                <a href="#">
+                    <img src="{{ asset('images/dc-logo.png') }}" alt="logo dc">
+                </a>
+            </div>
+            <div class="menu">
+                <ul class="text-uppercase">
+                    <li>
+                        <a href="#">characters</a>
+                    </li>
+                    <li>
+                        <a href="#">comics</a>
+                    </li>
+                    <li>
+                        <a href="#">movies</a>
+                    </li>
+                    <li>
+                        <a href="#">tv</a>
+                    </li>
+                    <li>
+                        <a href="#">games</a>
+                    </li>
+                    <li>
+                        <a href="#">collectibles</a>
+                    </li>
+                    <li>
+                        <a href="#">videos</a>
+                    </li>
+                    <li>
+                        <a href="#">fans</a>
+                    </li>
+                    <li>
+                        <a href="#">news</a>
+                    </li>
+                    <li>
+                        <a href="#">shop <span> &#9660; </span> </a>
+                    </li>
+                </ul>
 
+            </div>
+            <div class="search">
+
+            </div>
+        </div>
     </div>
 </header>
