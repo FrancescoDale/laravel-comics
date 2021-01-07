8659a2d2ddcc66fc2f93a4c6dbf3ac879f35e1aa
FrancescoDale <fd060862@gmail.com>
2021-01-07 15:59:59 +0100

    Layout e grafica header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel Comics</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
-
-    </head>
-    <body>
-        <h1 class="text-uppercase">provaaaaa</h1>
-    </body>
-</html>
+@section('content')
+    <h1>prova</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <div class="header-up">
+        <nav>
+            <ul>
+                <li>
+                    <a class="text-uppercase" href="#">dc power visa</a>
+                </li>
+                <li>
+                    <a class="text-uppercase" href="#">additional dc sites </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <div class="header-navbar">
+
+    </div>
+</header>
