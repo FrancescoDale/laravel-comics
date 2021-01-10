962e76417ac8051ad1a2fc0c78e1b803ff14446b
FrancescoDale <fd060862@gmail.com>
2021-01-10 22:45:01 +0100

    layout footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="footer-top container-fluid">
+        <div class="footer-top-inner container">
+                                   
+        </div>
+    </div>
+    <div class="footer-bottom container-fluid">
+
+    </div>
+</footer>
