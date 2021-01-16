8f5dbdb9b4034adfe537f6eb40ae34bb6a53a423
FrancescoDale <fd060862@gmail.com>
2021-01-16 15:49:08 +0100

    position absolute locandina fumetto

--- resources/views/comicpage.blade.php
+++ resources/views/comicpage.blade.php
@@ -5,9 +5,13 @@
 @section('content')
     @include('partials.jumbotron')
 
-    <section id="" class="container">
-        <img src="{{ $comic['thumb']}}" alt="{{ $comic['title'] }}">
-        <div>
+    <section id="">
+        <div class="comic-thumb-container">
+            <div class="container">
+                <img src="{{ $comic['thumb']}}" alt="{{ $comic['title'] }}">
+            </div>
+        </div>
+        <div class="container">
             <h1> {{ $comic['title'] }}</h1>
         </div>
     </section>
