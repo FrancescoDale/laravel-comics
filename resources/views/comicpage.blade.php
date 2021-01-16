fc1aa95c15f9e0550396adfc9222811d528ef68d
FrancescoDale <fd060862@gmail.com>
2021-01-16 16:27:28 +0100

    box pubblicità

--- resources/views/comicpage.blade.php
+++ resources/views/comicpage.blade.php
@@ -6,13 +6,23 @@
     @include('partials.jumbotron')
 
     <section id="">
-        <div class="comic-thumb-container">
+        <div class="comic-thumb-container container-fluid">
             <div class="container">
                 <img src="{{ $comic['thumb']}}" alt="{{ $comic['title'] }}">
             </div>
         </div>
-        <div class="container">
-            <h1> {{ $comic['title'] }}</h1>
+    </section>
+    <section id="main-comic-info" class="container">
+        <div class="info-row">
+            <div class="comic-info">
+                <h1 class="mt-2"> {{ $comic['title'] }}</h1>
+
+            </div>
+            <div class="adv-box text-right">
+                <p class="text-uppercase mt-2">advertisement</p>
+                <img src="{{ asset("/images/adv.jpg") }}" alt="advertisement">
+            </div>
         </div>
     </section>
+
 @endsection
