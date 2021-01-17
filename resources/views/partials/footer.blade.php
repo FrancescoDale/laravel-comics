fb60203a59d90dbe96dbda2ed7f5c378864bada0
FrancescoDale <fd060862@gmail.com>
2021-01-17 15:54:55 +0100

    struttura footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,10 +1,110 @@
 <footer>
     <div class="footer-top container-fluid">
         <div class="footer-top-inner container">
-                                   
+            <div class="footer-top-menu">
+                <h3 class="text-uppercase">dc comics</h3>
+                    <ul class="text-capitalize">
+                        <li>
+                            <a href="#">characters</a>
+                        </li>
+                        <li>
+                            <a href="#">comics</a>
+                        </li>
+                        <li>
+                            <a href="#">movies</a>
+                        </li>
+                        <li>
+                            <a href="#">tv</a>
+                        </li>
+                        <li>
+                            <a href="#">games</a>
+                        </li>
+                        <li>
+                            <a href="#">videos</a>
+                        </li>
+                        <li>
+                            <a href="#">news</a>
+                        </li>
+                    </ul>
+                    <h3 class="text-uppercase">shop</h3>
+                    <ul>
+                        <li>
+                            <a href="#">shop DC</a>
+                        </li>
+                        <li>
+                            <a href="#">shop DC collectibles</a>
+                        </li>
+                    </ul>
+            </div>
+            <div class="footer-top-menu">
+                <h3 class="text-uppercase">dc</h3>
+                <ul class="text-capitalize">
+                    <li>
+                        <a href="#">terms of use</a>
+                    </li>
+                    <li>
+                        <a href="#">privacy policy</a>
+                    </li>
+                    <li>
+                        <a href="#">ad choices</a>
+                    </li>
+                    <li>
+                        <a href="#">advertising</a>
+                    </li>
+                    <li>
+                        <a href="#">jobs</a>
+                    </li>
+                    <li>
+                        <a href="#">subscriptions</a>
+                    </li>
+                    <li>
+                        <a href="#">talent workshops</a>
+                    </li>
+                    <li>
+                        <a href="#">CPSC certificates</a>
+                    </li>
+                    <li>
+                        <a href="#">ratings</a>
+                    </li>
+                    <li>
+                        <a href="#">shop help</a>
+                    </li>
+                    <li>
+                        <a href="#">contact us</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-top-menu">
+                <h3 class="text-uppercase">sites</h3>
+                <ul class="text-capitalize">
+                    <li>
+                        <a href="#">DC</a>
+                    </li>
+                    <li>
+                        <a href="#">MAD magazine</a>
+                    </li>
+                    <li>
+                        <a href="#">DC kids</a>
+                    </li>
+                    <li>
+                        <a href="#">DC universe</a>
+                    </li>
+                    <li>
+                        <a href="#">DC power visa</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+        <div class="footer-top-notes container">
+            <p>All Site Content TM and © 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved.</p>
+            <p>
+                <a href="#">Cookies Settings</a>
+            </p>
         </div>
     </div>
     <div class="footer-bottom container-fluid">
+
 
     </div>
 </footer>
